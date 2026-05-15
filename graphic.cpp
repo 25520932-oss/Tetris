1236e7612b1e628f87b8cb94d5f2af452d84e9ca
25520926-BaAnKhuong <25520926@gm.uit.edu.vn>
2026-05-15 22:52:34 +0700

    tao mau sac cho block, them hieu ung

--- graphic.cpp
+++ graphic.cpp
@@ -1,43 +1,59 @@
 #include "graphic.h"
+#include <thread>
+#include <chrono>
 
 void initGraphics() {
 #ifdef _WIN32
-    // Ép Console dùng bảng mã UTF-8 (65001)
     SetConsoleOutputCP(65001);
-    SetConsoleCP(65001);
-
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD dwMode = 0;
     GetConsoleMode(hOut, &dwMode);
-    // Bật xử lý mã màu ANSI
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(hOut, dwMode);
 #endif
-    // Ẩn con trỏ chuột cho chuyên nghiệp
     std::cout << "\033[?25l";
 }
 
 void printCell(char cell) {
     switch (cell) {
-    case 'I': std::cout << "\033[96m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'O': std::cout << "\033[93m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'T': std::cout << "\033[95m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'S': std::cout << "\033[92m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'Z': std::cout << "\033[91m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'J': std::cout << "\033[94m" << BLOCK_CHAR << "\033[0m"; break;
-    case 'L': std::cout << "\033[38;5;208m" << BLOCK_CHAR << "\033[0m"; break;
-    case '#': std::cout << "\033[90m" << WALL_CHAR << "\033[0m"; break;
-    default:  std::cout << "\033[2m" << BG_CHAR << "\033[0m"; break;
+        case 'I': std::cout << "\033[96m" << BLOCK_CHAR << RESET; break;
+        case 'O': std::cout << "\033[93m" << BLOCK_CHAR << RESET; break;
+        case 'T': std::cout << "\033[95m" << BLOCK_CHAR << RESET; break;
+        case 'S': std::cout << "\033[92m" << BLOCK_CHAR << RESET; break;
+        case 'Z': std::cout << "\033[91m" << BLOCK_CHAR << RESET; break;
+        case 'J': std::cout << "\033[94m" << BLOCK_CHAR << RESET; break;
+        case 'L': std::cout << "\033[38;5;208m" << BLOCK_CHAR << RESET; break;
+        case 'G': std::cout << GHOST << "[]" << RESET; break; // Ký hiệu Ghost Piece
+        case 'F': std::cout << "\033[97m" << "██" << RESET; break; // Ký hiệu Flash
+        case '#': std::cout << "\033[90m" << WALL_CHAR << RESET; break;
+        default:  std::cout << "\033[2m" << BG_CHAR << RESET; break;
     }
 }
 
 void drawBoard(char board[20][15]) {
-    // Đưa con trỏ về đầu thay vì xóa màn hình (Mục tiêu: Animation mượt)
     std::cout << "\033[H";
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 15; j++) {
             printCell(board[i][j]);
         }
         std::cout << "\n";
+    }
+}
+
+// HIỆU ỨNG ĂN ĐIỂM (ANIMATION)
+void playClearAnimation(char board[20][15], int row) {
+    // Lưu lại trạng thái gốc của hàng
+    char originalRow[15];
+    for(int j=0; j<15; j++) originalRow[j] = board[row][j];
+
+    // Nháy sáng 2 lần
+    for (int flash = 0; flash < 2; flash++) {
+        for (int j = 1; j < 14; j++) board[row][j] = 'F'; // 'F' là Flash
+        drawBoard(board);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+        for (int j = 1; j < 14; j++) board[row][j] = ' '; 
+        drawBoard(board);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 }
--- graphic.h
+++ graphic.h
@@ -5,15 +5,23 @@
 #include <string>
 
 #ifdef _WIN32
-#include <windows.h>
+    #include <windows.h>
+    #define BLOCK_CHAR "\xe2\x96\x88\xe2\x96\x88" 
+    #define WALL_CHAR  "\xe2\x96\x92\xe2\x96\x92"
+    #define BG_CHAR    "\xc2\xb7 " 
+#else
+    #define BLOCK_CHAR "██"
+    #define WALL_CHAR  "▒▒"
+    #define BG_CHAR    "· "
 #endif
 
-// Sử dụng mã Escape cho ký tự Full Block (U+2588)
-#define BLOCK_CHAR "\xe2\x96\x88\xe2\x96\x88" 
-#define WALL_CHAR  "\xe2\x96\x92\xe2\x96\x92"
-#define BG_CHAR    "\xc2\xb7 " 
+// Mã màu bổ sung cho Animation
+const std::string RESET = "\033[0m";
+const std::string FLASH = "\033[97m\033[5m"; // Màu trắng + hiệu ứng nhấp nháy (nếu terminal hỗ trợ)
+const std::string GHOST = "\033[90m";        // Màu xám đậm cho bóng mờ
 
 void initGraphics();
 void drawBoard(char board[20][15]);
+void playClearAnimation(char board[20][15], int row); // Hiệu ứng ăn điểm
 
 #endif
