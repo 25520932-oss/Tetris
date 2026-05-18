ec7052fda28891cf85d31ce11eca4713b764561a
PhamLeChiCong <25520217@gm.uit.edu.vn>
2026-05-18 17:03:23 +0700

    them cac nut bam va title cho bang setting

--- UI.cpp
+++ UI.cpp
@@ -1,14 +1,11 @@
 #include "UI.h"
 #include "audio.h"
 
-// Định nghĩa object ui thật sự nằm ở đây
+
 UI ui; 
 
-// Móc object audio từ bên ngoài vào để chỉnh nhạc
 extern Audio gameAudio; 
 
-// Giả sử bro đang dùng mã phím ASCII hoặc enum gì đó
-// 72: Lên, 80: Xuống, 75: Trái, 77: Phải (Mã phím Windows Console)
 void UI::handleSettingInput(int keyCode) {
     if (!isMenuSettingOpen) return;
 
@@ -21,25 +18,25 @@ void UI::handleSettingInput(int keyCode) {
         if (keyCode == 75) { // Trái
             currentMusicVol--;
             if (currentMusicVol < 0) currentMusicVol = 0;
-            gameAudio.setMusicVolume(currentMusicVol * 12); 
+            gameAudio.setMusicVolume(currentMusicVol); 
         }
         else if (keyCode == 77) { // Phải
             currentMusicVol++;
             if (currentMusicVol > 10) currentMusicVol = 10;
-            gameAudio.setMusicVolume(currentMusicVol * 12);
+            gameAudio.setMusicVolume(currentMusicVol);
         }
     } 
     else {
         if (keyCode == 75) { // Trái
             currentSFXVol--;
             if (currentSFXVol < 0) currentSFXVol = 0;
-            gameAudio.setSFXVolume(currentSFXVol * 12);
+            gameAudio.setSFXVolume(currentSFXVol);
             gameAudio.playSFX(SoundEffect::MOVE); 
         }
         else if (keyCode == 77) { // Phải
             currentSFXVol++;
             if (currentSFXVol > 10) currentSFXVol = 10;
-            gameAudio.setSFXVolume(currentSFXVol * 12);
+            gameAudio.setSFXVolume(currentSFXVol);
             gameAudio.playSFX(SoundEffect::MOVE); 
         }
     }
