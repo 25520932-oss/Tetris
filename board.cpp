64d59570ff274a69b40c0c5906374a826d6178e9
Bui Trong Kien <25520932@gm.uit.edu.vn>
2026-05-22 21:57:01 +0700

    xoa ham dieu chinh am thanh vi chua phu hop

--- board.cpp
+++ board.cpp
@@ -16,19 +16,3 @@ void setCell(int x, int y, char value) {
 char getCell(int x, int y) {
     return isInside(x, y) ? board[x][y] : CELL_WALL;
 }
-void Audio::setMusicVolume(int volume) {
-    // Ép kiểu an toàn, đảm bảo volume không vượt quá 0 - 128
-    if (volume < 0) volume = 0;
-    if (volume > 128) volume = 128;
-    
-    // Hàm của SDL_mixer để chỉnh âm lượng nhạc nền
-    Mix_VolumeMusic(volume);
-}
-
-void Audio::setSFXVolume(int volume) {
-    if (volume < 0) volume = 0;
-    if (volume > 128) volume = 128;
-    
-    // Tham số -1 nghĩa là áp dụng mức âm lượng này cho TẤT CẢ các channel SFX hiện có
-    Mix_Volume(-1, volume);
-}
